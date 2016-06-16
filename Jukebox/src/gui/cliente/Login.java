9d413f925ead0a5cc4618f59bc79d67692f0e893
crp3 <caiorochapstrlrd@gmail.com>
2016-06-16 11:02:54 -0300

    Gente commitei aqui mas ainda faltam coisas(não consigo abrir janelas pelo botão D:)

--- Jukebox/src/gui/cliente/Login.java
+++ Jukebox/src/gui/cliente/Login.java
@@ -7,10 +7,16 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -30,10 +36,41 @@ public class Login extends JDialog {
 	 */
 	public Login() {
 		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
+		getContentPane().setLayout(null);
+		contentPanel.setBounds(0, 0, 434, 20);
 		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		getContentPane().add(contentPanel);
+		
+		JLabel lblLogin = new JLabel("Login:");
+		lblLogin.setBounds(42, 34, 83, 24);
+		getContentPane().add(lblLogin);
+		
+		textField = new JTextField();
+		textField.setBounds(118, 31, 172, 31);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("Senha:");
+		lblSenha.setBounds(42, 107, 46, 14);
+		getContentPane().add(lblSenha);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(118, 99, 172, 31);
+		getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnEntrar.setBounds(320, 35, 89, 27);
+		getContentPane().add(btnEntrar);
+		
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setBounds(320, 103, 89, 27);
+		getContentPane().add(btnCadastrar);
 	}
-
 }
