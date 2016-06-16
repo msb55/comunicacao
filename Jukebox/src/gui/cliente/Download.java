14f546bf0d5ba28b4e55e88fef8c2497c4a614b8
pedro lins <lins.pedro17@gmail.com>
2016-06-15 22:53:13 -0300

    fiz a classe de download :]

--- Jukebox/src/gui/cliente/Download.java
+++ Jukebox/src/gui/cliente/Download.java
@@ -7,6 +7,10 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JProgressBar;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Download extends JDialog {
 
@@ -29,11 +33,36 @@ public class Download extends JDialog {
 	 * Create the dialog.
 	 */
 	public Download() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 587, 388);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JProgressBar progressBarDownload = new JProgressBar();
+		progressBarDownload.setBounds(35, 91, 500, 37);
+		progressBarDownload.setValue(0);
+		contentPanel.add(progressBarDownload);
+		
+		JLabel lblNomeArquivo = new JLabel("Progresso...");
+		lblNomeArquivo.setBounds(35, 51, 87, 16);
+		contentPanel.add(lblNomeArquivo);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(438, 156, 97, 25);
+		contentPanel.add(btnCancelar);
+		
+		final JButton btnPlayOrPause = new JButton("Pausar"); //play \u25BA pause \u23F8
+		btnPlayOrPause.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (btnPlayOrPause.getText().equals("Pausar")) {
+					btnPlayOrPause.setText("Continuar");
+				} else {
+					btnPlayOrPause.setText("Pausar");
+				}
+			}
+		});
+		btnPlayOrPause.setBounds(35, 156, 97, 25);
+		contentPanel.add(btnPlayOrPause);
 	}
-
 }
